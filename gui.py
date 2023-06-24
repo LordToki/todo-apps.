904262fc7edc6b21f1f9b79da1441f448fad590b
TOKI <galihclassroom@gmail.com>
2023-06-24 17:48:28 +0700

    Add the "add", "close"(because its python 3.8)

--- gui.py
+++ gui.py
@@ -1,9 +1,27 @@
 import func
 import PySimpleGUI as sg
-label = sg.Text("Type in to do")
-input_box = sg.InputText(tooltip="Enter todo")
-add_button = sg.Button("add")
 
-window = sg.Window("My todo APP", layout=[[label], [input_box, add_button]])
-window.read()
-window.close()
+label = sg.Text("Type in to do")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
+add_button1 = sg.Button("add")
+add_button2 = sg.Button("close")
+window = sg.Window("My todo APP",
+                   layout=[[label],
+                           [add_button2],
+                           [input_box, add_button1]],
+                   font=("helvetica",))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+
+    if "add" in event:
+        todos = func.get_todos()
+        new_todo = values["todo"] + "\n"
+        todos.append(new_todo)
+        func.write_todos(todos)
+    elif "close" in event:
+        break
+
+window.close
