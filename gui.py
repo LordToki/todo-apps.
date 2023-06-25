13768a10632dab3b5d2d4d990944a9856f1f310b
TOKI <galihclassroom@gmail.com>
2023-06-25 17:42:35 +0700

    Added edit and display button

--- gui.py
+++ gui.py
@@ -1,27 +1,42 @@
 import func
-import PySimpleGUI as sg
+import PySimpleGUI as SG
 
-label = sg.Text("Type in to do")
-input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button1 = sg.Button("add")
-add_button2 = sg.Button("close")
-window = sg.Window("My todo APP",
+label = SG.Text("Type in to do")
+input_box = SG.InputText(tooltip="Enter todo", key="todo")
+add_button1 = SG.Button("Add")
+add_button2 = SG.Button("Close")
+list_box = SG.Listbox(values=func.get_todos(), key="todos",
+                      enable_events=True, size=[45, 10])
+edit_button = SG.Button("Edit")
+window = SG.Window("My todo APP",
                    layout=[[label],
                            [add_button2],
-                           [input_box, add_button1]],
+                           [input_box, add_button1], [list_box, edit_button]],
                    font=("helvetica",))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
-
-    if "add" in event:
+    print(1, event)
+    print(2, values)
+    print(3, values["todos"])
+    if "Add" in event:
         todos = func.get_todos()
         new_todo = values["todo"] + "\n"
         todos.append(new_todo)
         func.write_todos(todos)
-    elif "close" in event:
+        window["todos"].update(values=todos)
+    if "Edit" in event:
+        todo_to_edit = values["todos"][0]
+        new_todo = values["todo"]
+
+        todos = func.get_todos()
+        index = todos.index(todo_to_edit)
+        todos[index] = new_todo + "\n"
+        func.write_todos(todos)
+        window["todos"].update(values=todos)
+    if "todos" in event:
+        window["todo"].update(value=values["todos"][0])
+    elif "Close" in event:
         break
 
 window.close
